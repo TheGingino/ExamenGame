8c59a25d19bc483099cf02792683a3e5c844f733
Kiana Hiemstra <113173667+SSeinaru@users.noreply.github.com>
2026-03-19 15:33:55 +0100

    Started on the Level selector screen

---
+++ Match-3-Team2/Assets/scripts/LevelSelector/LevelSelector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    [SerializeField] private int _buttonID;
+
+    private void LevelSelect()
+    {
+        switch (_buttonID)
+        {
+            //case :
+
+            //break;
+
+
+
+        }
+    }
+
+}
