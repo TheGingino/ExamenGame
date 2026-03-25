41d836326328abd32f04f44a1cc53f04424a260b
Gino Schaap <35615@ma-web.nl>
2026-03-25 14:49:40 +0100

    Added LevelUnlocking and Saving Progress

---
+++ Match-3-Team2/Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveSystem : MonoBehaviour
+{
+    [SerializeField] private int levelProgress;
+    [SerializeField] private string saveKey = "LevelProgress";
+    [SerializeField] private Button[] levelButtons;
+    
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("LevelProgress"))
+        {
+            levelProgress = PlayerPrefs.GetInt(saveKey, 0);
+            Debug.Log($"Loaded level progress: {levelProgress}");
+            UpdateLevelButtons();
+        }
+    }
+
+    private void UpdateLevelButtons()
+    {
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            levelButtons[i].interactable = i <= levelProgress;
+        }
+    }
+
+    public void UpdateLevelProgress()
+    {
+        levelProgress++;
+        PlayerPrefs.SetInt(saveKey, levelProgress);
+        UpdateLevelButtons();
+    }
+
+    public void ResetProgress()
+    {
+        levelProgress = 0;
+        PlayerPrefs.SetInt(saveKey, levelProgress);
+        UpdateLevelButtons();
+    }
+
+}
