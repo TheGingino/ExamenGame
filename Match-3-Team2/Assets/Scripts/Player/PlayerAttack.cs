28f726c2d875caeb900a4f37ef285d3e9bd64212
Nikki van Wijngaarden <nikki.vw666@gmail.com>
2026-03-27 14:20:59 +0100

    Final push for the linking
    and added charged

--- Match-3-Team2/Assets/Scripts/Player/PlayerAttack.cs
+++ Match-3-Team2/Assets/Scripts/Player/PlayerAttack.cs
@@ -25,7 +25,7 @@ public class PlayerAttack : MonoBehaviour
       }
    }
 
-   void DoDamage()
+   public void DoDamage()
    {
       if (CombatMeter.Instance.UseCharge(TileType.Damage))
       {
@@ -35,7 +35,7 @@ public class PlayerAttack : MonoBehaviour
    }
 
 
-   void SpecialAttack()
+   public void SpecialAttack()
    {
       if (CombatMeter.Instance.UseCharge(TileType.Special))
       {
