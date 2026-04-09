a88535fafddaf6ee1363e76a3738df09edc86621
Gino Schaap <35615@ma-web.nl>
2026-04-09 14:01:08 +0200

    Toggleable fps on screen

--- Match-3-Team2/Assets/Scripts/Menu/PauseScreen.cs
+++ Match-3-Team2/Assets/Scripts/Menu/PauseScreen.cs
@@ -2,17 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseScreen : MonoBehaviour
 {
     [SerializeField] private GameObject gridSystem;
 
     [SerializeField] private GameObject fpsText;
+    
+    [SerializeField] private Toggle _toggle;
     private bool showingFPS = false;
 
     private void Start()
     {
-        fpsText.SetActive(false);
+        fpsText.SetActive(showingFPS);
     }
 
     public void ResumeGame()
@@ -29,6 +32,18 @@ public class PauseScreen : MonoBehaviour
     public void ToggleFPS(bool toggle)
     {
         showingFPS = toggle;
-        fpsText.SetActive(showingFPS);
+        if (_toggle.isOn == toggle)
+        {
+            Debug.Log("The Toggle is true " + _toggle.isOn);
+            showingFPS = true;
+            fpsText.SetActive(showingFPS);
+        }
+        else if (_toggle.isOn == !toggle)
+        {
+            Debug.Log("The Toggle is false " + _toggle.isOn);
+            showingFPS = false;
+            fpsText.SetActive(showingFPS);
+            //run code here if false
+        }
     }
 }
